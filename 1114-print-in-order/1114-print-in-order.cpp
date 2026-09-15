6355ad08f9783477161e62ec19a5a5f5a2193b9c
dishant Garg <dishantgarg050@gmailo.com>
2026-09-15 23:14:21 +0530

    Time: 6 ms (55.49%), Space: 9.3 MB (100%) - LeetHub

--- 1114-print-in-order/1114-print-in-order.cpp
+++ 1114-print-in-order/1114-print-in-order.cpp
@@ -1,28 +1,27 @@
-#include <iostream>
-#include <thread>
-#include <condition_variable>
-
-using namespace std;
 
 class Foo {
     mutex m;
     condition_variable cond;
-    int turn = 0;
+    int turn;//declare turn variable that indicate whose turn to enter in C.S
 
 public:
+      Foo(){
+        turn=0;// initialize
+      }
 
     void first(function<void()> printFirst) {
-        printFirst();
+        unique_lock<mutex> lock(m);// unique_lock manage mutex
+        printFirst();//C.S-->print first
 
-        turn = 1;
-        cond.notify_all();
+        turn = 1;// after excute print operation then turn of next thread/other thread
+        cond.notify_all();//signal to all other threads for check their turn or not, if its their turn then wake up
     }
 
     void second(function<void()> printSecond) {
         unique_lock<mutex> lock(m);
 
-        while (turn != 1) {
-            cond.wait(lock);
+        while (turn != 1) {// if not their turn then block/ stuck in while loop
+        cond.wait(lock);// and wait on cond
         }
 
         printSecond();
