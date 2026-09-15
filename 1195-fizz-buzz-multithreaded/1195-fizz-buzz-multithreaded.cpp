c3cd180cf574b72439797fb49dee46bbe244723d
dishant Garg <dishantgarg050@gmailo.com>
2026-09-16 01:37:37 +0530

    Time: 0 ms (100%), Space: 9.5 MB (33.65%) - LeetHub

---
+++ 1195-fizz-buzz-multithreaded/1195-fizz-buzz-multithreaded.cpp
@@ -0,0 +1,84 @@
+
+class FizzBuzz {
+    int n;
+    mutex m;
+    condition_variable c;
+    int i;
+
+public:
+    FizzBuzz(int n) {
+        this->n = n;
+        i = 1;
+    }
+
+    // printFizz() outputs "fizz".
+    void fizz(function<void()> printFizz) {
+        while (i <= n) {
+        
+                unique_lock<mutex> lock(m);
+                while (i <= n && (((i % 3) == 0) && ((i % 5) != 0)) == 0) {
+                    c.wait(lock);
+                }
+                if (i <= n) {
+                    printFizz();
+                    ++i;
+                }
+
+                c.notify_all();
+        
+        }
+    }
+
+    // printBuzz() outputs "buzz".
+    void buzz(function<void()> printBuzz) {
+        while (i <= n) {
+    
+                unique_lock<mutex> lock(m);
+                while (i <= n && (((i % 5) == 0) && ((i % 3) != 0)) == 0) {
+                    c.wait(lock);
+                }
+                if (i <= n) {
+                    printBuzz();
+                    ++i;
+                }
+
+                c.notify_all();
+            
+        }
+    }
+
+    // printFizzBuzz() outputs "fizzbuzz".
+    void fizzbuzz(function<void()> printFizzBuzz) {
+        while (i <= n) {
+            
+                unique_lock<mutex> lock(m);
+                while (i <= n && (((i % 5) == 0) && ((i % 3) == 0)) == 0) {
+                    c.wait(lock);
+                }
+                if (i <= n) {
+                    printFizzBuzz();
+                    ++i;
+                }
+
+                c.notify_all();
+            
+        }
+    }
+
+    // printNumber(x) outputs "i", where i is an integer.
+    void number(function<void(int)> printNumber) {
+        while (i <= n) {
+            
+                unique_lock<mutex> lock(m);
+                while (i <= n &&(((i % 5) != 0) &&((i % 3) != 0)) == 0) {
+                    c.wait(lock);
+                }
+                if (i <= n) {
+                    printNumber(i++);
+                }
+
+                c.notify_all();
+            
+        }
+    }
+};
