54e34a2c2e1e5e2f2634a0392475b6108fe28ab9
dishant Garg <dishantgarg050@gmailo.com>
2026-09-17 03:05:12 +0530

    Time: 45 ms (14.76%), Space: 18.4 MB (73.99%) - LeetHub

--- 1226-the-dining-philosophers/1226-the-dining-philosophers.cpp
+++ 1226-the-dining-philosophers/1226-the-dining-philosophers.cpp
@@ -1,67 +1,26 @@
-#include <mutex>
-#include <condition_variable>
-#include <functional>
+//use deadlock avoid rule- (where allow ph to pick his right and left fork when both are available)-->only one ph pick both forks that timme and other wait-->make critical section to pick both nd alsp critical section to put both fork
 
-using namespace std;
-
-
-// ---------------- USER DEFINED SEMAPHORE ----------------
-
-class Semaphore {
-
-private:
-    int count;
-    mutex mtx;
-    condition_variable cv;
-
-public:
-
-    Semaphore() {
-        count = 0;
-    }
-
-    void setCount(int a) {
-        count = a;
-    }
-
-    void wait() {
-        unique_lock<mutex> lock(mtx);
-
-        count--;
-
-        while (count < 0) {
-            cv.wait(lock);
-        }
-    }
-
-    void signal() {
-        unique_lock<mutex> lock(mtx);
-
-        count++;
-
-        if (count <= 0) {
-            cv.notify_one();
-        }
-    }
-};
-
-
-// ---------------- DINING PHILOSOPHERS ----------------
-
+// ---------------- in-built SEMAPHORE ----------------
 class DiningPhilosophers {
+    counting_semaphore<1> fork[5] = {
+    counting_semaphore<1>(1),
+    counting_semaphore<1>(1),
+    counting_semaphore<1>(1),
+    counting_semaphore<1>(1),
+    counting_semaphore<1>(1)
 
-private:
+};
+mutex m;
+//  binary_semaphore fork[5] = {   
+//         binary_semaphore(1),   
+//         binary_semaphore(1),   
+//         binary_semaphore(1),   
+//         binary_semaphore(1),   
+//         binary_semaphore(1)   
+//     };   
 
-    Semaphore fork[5];
-    mutex m;
 public:
-
-    DiningPhilosophers() {
-
-        for (int i = 0; i < 5; i++) {
-            fork[i].setCount(1);
-        }
-    }
+    DiningPhilosophers() {}
 
     void wantsToEat(
         int philosopher,
@@ -71,45 +30,429 @@ public:
         function<void()> putLeftFork,
         function<void()> putRightFork
     ) {
-
-        int left = philosopher;
-        int right = (philosopher + 1) % 5;
-
-
-        // EVEN philosopher
-        // Left -> Right
-
-        if (philosopher % 2 == 0) {
-            unique_lock<mutex> lock(m);
-            fork[left].wait();
-            fork[right].wait();
-
-            pickLeftFork();
-            pickRightFork();
-        }
-
-
-        // ODD philosopher
-        // Right -> Left
-
-        else {
-            unique_lock<mutex> lock(m);
-            fork[right].wait();
-            fork[left].wait();
-
-            pickRightFork();
-            pickLeftFork();
-        }
-
-
+        unique_lock<mutex> lock(m);
+        // pick in the clockwise order
+         pickLeftFork();
+        pickRightFork();
+        fork[(philosopher + 1) % 5].acquire();// left
+        fork[philosopher].acquire();// right
         eat();
 
-
+       // Release both forks
         putLeftFork();
         putRightFork();
-
-
-        fork[left].signal();
-        fork[right].signal();
+        fork[(philosopher + 1) % 5].release();
+        fork[philosopher].release();
     }
 };
+
+
+// ---------------- user defined SEMAPHORE ----------------
+// class Semaphore {
+
+// private:
+//     int count;
+//     mutex mtx;
+//     condition_variable cv;
+
+// public:
+
+//     // Default constructor
+//     Semaphore() {
+//      count=0;
+//    }
+
+//     void setCount(int a) {
+//         count = a;
+//     }
+
+//     void wait() {
+//         unique_lock<mutex> lock(mtx);
+
+//         count--;
+
+//         if (count < 0) {
+//             cv.wait(lock);
+//         }
+//     }
+
+//     void signal() {
+//         unique_lock<mutex> lock(mtx);
+
+//         count++;
+
+//         if (count <= 0) {
+//             cv.notify_one();
+//         }
+//     }
+// };
+
+
+// ---------------- DINING PHILOSOPHERS ----------------
+
+// class DiningPhilosophers {
+
+// private:
+//     Semaphore fork[5];
+//     mutex m;
+// public:
+
+//     DiningPhilosophers() {
+
+//         for (int i = 0; i < 5; ++i) {
+//             fork[i].setCount(1);
+//         }
+//     }
+
+//     void wantsToEat(
+//         int philosopher,
+//         function<void()> pickLeftFork,
+//         function<void()> pickRightFork,
+//         function<void()> eat,
+//         function<void()> putLeftFork,
+//         function<void()> putRightFork
+//     ) {
+//         unique_lock<mutex> lock(m);
+//         fork[philosopher].wait();
+//         fork[(philosopher + 1) % 5].wait();
+
+//         pickLeftFork();
+//         pickRightFork();
+
+//         eat();
+
+//         putLeftFork();
+//         putRightFork();
+
+//         fork[philosopher].signal();
+//         fork[(philosopher + 1) % 5].signal();
+//     }
+// };
+
+ 
+// --> At most 4 philosophers
+// Allow only 4 philosophers to try to pick forks at the same time. A counting_semaphore<4> acts as a “room” semaphore.
+
+//  class DiningPhilosophers {
+
+// private:
+//     // Each fork can be held by only one philosopher
+//     counting_semaphore<1> fork[5] = {
+//         counting_semaphore<1>(1),
+//         counting_semaphore<1>(1),
+//         counting_semaphore<1>(1),
+//         counting_semaphore<1>(1),
+//         counting_semaphore<1>(1)
+//     };
+
+//     // At most 4 philosophers can try to pick forks
+//     counting_semaphore<4> room{4};
+
+// public:
+
+//     DiningPhilosophers() {}
+
+//     void wantsToEat(
+//         int philosopher,
+//         function<void()> pickLeftFork,
+//         function<void()> pickRightFork,
+//         function<void()> eat,
+//         function<void()> putLeftFork,
+//         function<void()> putRightFork
+//     ) {
+
+//         // Allow maximum 4 philosophers into critical section
+//         room.acquire();
+
+//         // Pick both forks
+//         fork[philosopher].acquire();
+//         fork[(philosopher + 1) % 5].acquire();
+
+//         pickLeftFork();
+//         pickRightFork();
+
+//         eat();
+
+//         putLeftFork();
+//         putRightFork();
+
+//         // Release both forks
+//         fork[philosopher].release();
+//         fork[(philosopher + 1) % 5].release();
+
+//         // Leave room
+//         room.release();
+//     }
+// };
+
+
+// // ---------------- USER DEFINED SEMAPHORE ----------------
+
+// class Semaphore {
+
+// private:
+//     int count;
+//     mutex mtx;
+//     condition_variable cv;
+
+// public:
+
+//     Semaphore() {
+//         count = 0;
+//     }
+
+//     void setCount(int a) {
+//         count = a;
+//     }
+
+//     void wait() {
+//         unique_lock<mutex> lock(mtx);
+
+//         count--;
+
+//         while (count < 0) {
+//             cv.wait(lock);
+//         }
+//     }
+
+//     void signal() {
+//         unique_lock<mutex> lock(mtx);
+
+//         count++;
+
+//         if (count <= 0) {
+//             cv.notify_one();
+//         }
+//     }
+// };
+
+
+// // ---------------- DINING PHILOSOPHERS ----------------
+
+// class DiningPhilosophers {
+
+// private:
+
+//     Semaphore fork[5];
+
+//     // Maximum 4 philosophers can try to pick forks
+//     Semaphore table;
+// mutex m;
+// public:
+
+//     DiningPhilosophers() {
+
+//         for (int i = 0; i < 5; i++) {
+//             fork[i].setCount(1);
+//         }
+
+//         table.setCount(4);
+//     }
+
+//     void wantsToEat(
+//         int philosopher,
+//         function<void()> pickLeftFork,
+//         function<void()> pickRightFork,
+//         function<void()> eat,
+//         function<void()> putLeftFork,
+//         function<void()> putRightFork
+//     ) {
+//         unique_lock<mutex> lock(m);
+//         // Only 4 philosophers can try to pick forks
+//         table.wait();
+
+//         // Pick both forks
+//         fork[philosopher].wait();
+//         fork[(philosopher + 1) % 5].wait();
+
+//         pickLeftFork();
+//         pickRightFork();
+
+//         eat();
+
+//         putLeftFork();
+//         putRightFork();
+
+//         // Release both forks
+//         fork[philosopher].signal();
+//         fork[(philosopher + 1) % 5].signal();
+
+//         // Leave table
+//         table.signal();
+//     }
+// };
+// -->odd even rule
+// Even philosopher -left fork first, right fork second
+// Odd philosopher -right fork first, left fork second
+
+// class DiningPhilosophers {
+
+// private:
+
+//     counting_semaphore<1> fork[5] = {
+//         counting_semaphore<1>(1),
+//         counting_semaphore<1>(1),
+//         counting_semaphore<1>(1),
+//         counting_semaphore<1>(1),
+//         counting_semaphore<1>(1)
+//     };
+
+// public:
+
+//     DiningPhilosophers() {}
+
+//     void wantsToEat(
+//         int philosopher,
+//         function<void()> pickLeftFork,
+//         function<void()> pickRightFork,
+//         function<void()> eat,
+//         function<void()> putLeftFork,
+//         function<void()> putRightFork
+//     ) {
+
+//         int left = philosopher;
+//         int right = (philosopher + 1) % 5;
+
+//         if (philosopher % 2 == 0) {
+
+//             // Even philosopher:
+//             // Left -> Right
+//             fork[left].acquire();
+//             fork[right].acquire();
+
+//             pickLeftFork();
+//             pickRightFork();
+
+//         } else {
+
+//             // Odd philosopher:
+//             // Right -> Left
+//             fork[right].acquire();
+//             fork[left].acquire();
+
+//             pickRightFork();
+//             pickLeftFork();
+//         }
+
+//         eat();
+
+//         putLeftFork();
+//         putRightFork();
+
+//         fork[left].release();
+//         fork[right].release();
+//     }
+// };
+
+
+// // ---------------- USER DEFINED SEMAPHORE ----------------
+
+// class Semaphore {
+
+// private:
+//     int count;
+//     mutex mtx;
+//     condition_variable cv;
+
+// public:
+
+//     Semaphore() {
+//         count = 0;
+//     }
+
+//     void setCount(int a) {
+//         count = a;
+//     }
+
+//     void wait() {
+//         unique_lock<mutex> lock(mtx);
+
+//         count--;
+
+//         while (count < 0) {
+//             cv.wait(lock);
+//         }
+//     }
+
+//     void signal() {
+//         unique_lock<mutex> lock(mtx);
+
+//         count++;
+
+//         if (count <= 0) {
+//             cv.notify_one();
+//         }
+//     }
+// };
+
+
+// // ---------------- DINING PHILOSOPHERS ----------------
+
+// class DiningPhilosophers {
+
+// private:
+
+//     Semaphore fork[5];
+//     mutex m;
+// public:
+
+//     DiningPhilosophers() {
+
+//         for (int i = 0; i < 5; i++) {
+//             fork[i].setCount(1);
+//         }
+//     }
+
+//     void wantsToEat(
+//         int philosopher,
+//         function<void()> pickLeftFork,
+//         function<void()> pickRightFork,
+//         function<void()> eat,
+//         function<void()> putLeftFork,
+//         function<void()> putRightFork
+//     ) {
+
+//         int left = philosopher;
+//         int right = (philosopher + 1) % 5;
+
+
+//         // EVEN philosopher
+//         // Left -> Right
+
+//         if (philosopher % 2 == 0) {
+//             unique_lock<mutex> lock(m);
+//             fork[left].wait();
+//             fork[right].wait();
+
+//             pickLeftFork();
+//             pickRightFork();
+//         }
+
+
+//         // ODD philosopher
+//         // Right -> Left
+
+//         else {
+//             unique_lock<mutex> lock(m);
+//             fork[right].wait();
+//             fork[left].wait();
+
+//             pickRightFork();
+//             pickLeftFork();
+//         }
+
+
+//         eat();
+
+
+//         putLeftFork();
+//         putRightFork();
+
+
+//         fork[left].signal();
+//         fork[right].signal();
+//     }
+// };
+
+// BY ALL DEADLOCK AVOIDANCE RULE
+// ==> FINALLY, multiple threads/ph exexute/eat at same time and some are execute/eat one by one -SO,NO DEADLOCK OCCUR
